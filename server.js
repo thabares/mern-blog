68933305e29edc41e2f5721911878aa97239bb50
thabares <thabaresnazeer.1995@gmail.com>
2024-10-11 20:10:20 +0530

    order of the server file updated and with build path of the frontend updated.

--- server.js
+++ server.js
@@ -8,13 +8,14 @@ const path = require('path');
 const dotenv = require('dotenv');
 const cookieParser = require('cookie-parser');
 
+// Load environment variables
 dotenv.config();
+
+// Initialize express app
 const app = express();
 
+// Use JSON parsing with limit
 app.use(express.json({ limit: '10mb' }));
-
-// Connect to the database
-connectDB();
 
 // Use CORS
 const corsOptions = {
@@ -22,21 +23,27 @@ const corsOptions = {
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true, // Allows the server to accept cookies
 };
-
-app.use(cookieParser());
 app.use(cors(corsOptions));
-app.use(express.json());
+
+// Use Cookie Parser
+app.use(cookieParser());
+
+// Connect to the database
+connectDB();
+
+// Define API routes
 app.use('/api/auth', authRoutes);
 app.use('/api/post', postRoutes);
 app.use('/api', uploadRoutes);
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, '../client/build')));
+app.use(express.static(path.join(__dirname, './frontend/build')));
 
-// The "catchall" handler: for any request that doesn't match the above, send back the React app's index.html.
+// Catch-all route to serve the React app for any non-API request
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  res.sendFile(path.join(__dirname, './frontend/build/index.html'));
 });
 
+// Start the server
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
